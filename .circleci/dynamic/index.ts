ed3d6c602b81d19a56fc0e9e2e9ec103ce52f7a3
Ryan Kienstra <kienstraryan@gmail.com>
2022-09-24 15:01:03 -0500

    Add a .gitignore for dynamicConfig.yml

--- .circleci/dynamic/index.ts
+++ .circleci/dynamic/index.ts
@@ -12,62 +12,60 @@ const e2eExecutor = new CircleCI.executors.MachineExecutor('large', 'ubuntu-2004
 const nodeOrb = new CircleCI.orb.OrbImport('node', 'circleci', 'node', '5.0');
 const phpOrb = new CircleCI.orb.OrbImport('php', 'circleci', 'php', '1.1');
 
-workflow
-  .addJob(
-    new CircleCI.Job(
-      'js-build',
-      nodeExecutor,
-      [
-        new CircleCI.commands.Checkout(),
-        new CircleCI.commands.Run( {
-          command: nodeOrb.commands['install-packages']?.name,
-        } ),
-        new CircleCI.commands.Run( {
-          name: 'Running JS linting and unit test',
-          command: `npm run lint:js \n npm run test:js`,
-        } ),
-      ]
-    )
+const jobs = [
+  new CircleCI.Job(
+    'js-build',
+    nodeExecutor,
+    [
+      new CircleCI.commands.Checkout(),
+      new CircleCI.commands.Run( {
+        command: nodeOrb.commands['install-packages']?.name,
+      } ),
+      new CircleCI.commands.Run( {
+        name: 'Running JS linting and unit test',
+        command: `npm run lint:js \n npm run test:js`,
+      } ),
+    ]
+  ),
+  new CircleCI.Job(
+    'php-lint',
+    phpExecutor,
+    [
+      new CircleCI.commands.Checkout(),
+      new CircleCI.commands.Run( { command: 'composer i' } ),
+      new CircleCI.commands.Run( { command: 'composer lint' } ),
+    ]
+  ),
+  new CircleCI.Job(
+    'php-test',
+    phpExecutor,
+    [
+      new CircleCI.commands.Checkout(),
+      new CircleCI.commands.Run( { command: 'composer i' } ),
+      new CircleCI.commands.Run( { command: 'composer test' } ),
+    ]
+  ),
+  new CircleCI.Job(
+    'e2e-test',
+    e2eExecutor,
+    [
+      new CircleCI.commands.Checkout(),
+      new CircleCI.commands.Run( {
+        command: 'npm ci',
+      } ),
+      new CircleCI.commands.Run( {
+        name: 'Running e2e tests',
+        command: 'npm run wp-env start && npm run test:e2e',
+      } ),
+      new CircleCI.commands.StoreArtifacts({ path: 'artifacts' }),
+    ]
   )
-  .addJob(
-    new CircleCI.Job(
-      'php-lint',
-      phpExecutor,
-      [
-        new CircleCI.commands.Checkout(),
-        new CircleCI.commands.Run( { command: 'composer i' } ),
-        new CircleCI.commands.Run( { command: 'composer lint' } ),
-      ]
-    )
-  )
-  .addJob(
-    new CircleCI.Job(
-      'php-test',
-      phpExecutor,
-      [
-        new CircleCI.commands.Checkout(),
-        new CircleCI.commands.Run( { command: 'composer i' } ),
-        new CircleCI.commands.Run( { command: 'composer test' } ),
-      ]
-    )
-  )
-  .addJob(
-    new CircleCI.Job(
-      'e2e-test',
-      e2eExecutor,
-      [
-        new CircleCI.commands.Checkout(),
-        new CircleCI.commands.Run( {
-          command: 'npm ci',
-        } ),
-        new CircleCI.commands.Run( {
-          name: 'Running e2e tests',
-          command: 'npm run wp-env start && npm run test:e2e',
-        } ),
-        new CircleCI.commands.StoreArtifacts({ path: 'artifacts' }),
-      ]
-    )
-  );
+];
+
+jobs.forEach((job) => {
+  config.addJob(job)
+  workflow.addJob(job)
+});
 
 fs.writeFile(
   './dynamicConfig.yml',
